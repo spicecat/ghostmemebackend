214b53617502658de13463a69591180a764174d2
“spicecat” <ateh0701@gmail.com>
2021-07-24 16:18:30 -0500

    added blank avatar

--- services/userServices.js
+++ services/userServices.js
@@ -15,34 +15,35 @@ const nullifyUndefined = obj => Object.fromEntries(Object.entries(obj).map(v => 
 
 const register = async (req, res, next) => {
     const url = baseUrl
-    try {
-        const { name, email, phone, username, imageBase64, password } = req.body
-        const response = await superagent.post(url, nullifyUndefined({ name, email, phone, username, imageBase64: null })).set('key', apiKey)
-        const { user } = response.body
-        console.log(user)
-        const hashedPassword = await bycrypt.hash(password, 10)
-        await new userModel({ user_id: user.user_id, password: hashedPassword, imageBase64 }).save()
-        next()
-    } catch (err) {
+    const { name, email, phone, username, imageBase64, password } = req.body
+    try { var response = await superagent.post(url, nullifyUndefined({ name, email, phone, username, imageBase64: null })).set('key', apiKey) }
+    catch (err) {
         if (err.status === 555) setTimeout(async () => { await register(req, res, next) }, 1500)
         else res.status(400).send({ error: err.response.body.error })
+        return
     }
+    const { user } = response.body
+    console.log(user)
+    const hashedPassword = await bycrypt.hash(password, 10)
+    await new userModel({ user_id: user.user_id, password: hashedPassword, imageBase64 }).save()
+    next()
 }
 
 const getUser = async username => {
     const url = baseUrl + '/' + username.split('/', 1)
+    console.log(url)
     try {
-        console.log(url)
-        const response = await superagent.get(url).set('key', apiKey)
-        const { user } = response.body
-        console.log(user)
-        return user
+        var response = await superagent.get(url).set('key', apiKey)
     } catch (err) {
         if (err.status === 555) {
             const retry = async () => new Promise(resolve => setTimeout(() => resolve(getUser(username)), 1500))
             return await retry()
-        } else return {}
+        }
+        return {}
     }
+    const { user } = response.body
+    console.log(user)
+    return user
 }
 
 const login = async (req, res, next) => {
@@ -56,10 +57,7 @@ const login = async (req, res, next) => {
             else res.status(401).send({ error: 'Bad credentials' })
         }
         else res.status(401).send({ error: 'Bad credentials' })
-    } catch (err) {
-        if (err.status === 555) setTimeout(async () => { await login(req, res, next) }, 1500)
-        else res.status(400).send({ error: err.response.body.error })
-    }
+    } catch (err) { res.status(400).send({ error: 'Bad input' }) }
 }
 
 module.exports = { register, login }

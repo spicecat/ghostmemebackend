5561072d94b9f959999d4d631eebb23da4a10d26
“spicecat” <ateh0701@gmail.com>
2021-08-09 22:43:41 -0500

    increased request size limit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use((req, res, next) => {
     res.removeHeader('X-Powered-By')
     next()
 })
-app.use(bodyParser.json({ limit: '500kb' }))
+app.use(bodyParser.json({ limit: '1.2mb' }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 

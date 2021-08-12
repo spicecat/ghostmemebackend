67f09d164164f5983fea98de8704b045ad5c0e7d
“spicecat” <ateh0701@gmail.com>
2021-08-12 09:12:59 -0500

    moved imageBase64 to api

--- models/User.js
+++ models/User.js
@@ -4,8 +4,7 @@ const UserSchema = mongoose.Schema({
     user_id: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    imageBase64: { type: String }
+    password: { type: String, required: true }
 })
 
 module.exports = mongoose.model('user', UserSchema)
